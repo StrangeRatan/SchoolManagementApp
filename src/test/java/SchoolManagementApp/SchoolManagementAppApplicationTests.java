9459fda596de6c3d29f0e77b4333400349bfb7ac
Ratan <ratanyadav405@gmail.com>
2025-08-23 18:26:16 +0530

    Initializr Project setup form Spring Boot Initializr

---
+++ src/test/java/SchoolManagementApp/SchoolManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package SchoolManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

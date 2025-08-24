29d96fde5a2c22b21c52b97c45ef45bbd43d5604
Ratan <ratanyadav405@gmail.com>
2025-08-24 11:42:52 +0530

    Creating User Service class

--- src/main/java/SchoolManagementApp/Service/UserService.java
+++ src/main/java/SchoolManagementApp/Service/UserService.java
@@ -1,4 +1,45 @@
 package SchoolManagementApp.Service;
 
+import SchoolManagementApp.DTO.UserEntityDto;
+import SchoolManagementApp.DTO.UserEntityDtoSignup;
+import SchoolManagementApp.Entity.UserEntity;
+import SchoolManagementApp.Repository.UserEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserEntityRepository userEntityRepository;
+
+    public void createUser(UserEntityDtoSignup newUser){
+        try{
+            UserEntityDtoSignup user=newUser;
+            UserEntity adduser=new UserEntity();
+            adduser.setUsername(user.getUsername());
+            adduser.setPassword(user.getPassword());
+            adduser.setRole("USER");
+            userEntityRepository.save(adduser);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public List<UserEntityDto> getAllUsers(){
+        List<UserEntity> all=userEntityRepository.findAll();
+        List<UserEntityDto> alluser=new ArrayList<>();
+
+        for(UserEntity user: all){
+            UserEntityDto userEntityDto=new UserEntityDto();
+            userEntityDto.setId(user.getId());
+            userEntityDto.setUsername(user.getUsername());
+            userEntityDto.setRole(user.getRole());
+            alluser.add(userEntityDto);
+        }
+        return alluser;
+    }
+
 }

3134695b94ff3ec3832d4e5730302433aa874a3e
Ratan <ratanyadav405@gmail.com>
2025-08-24 11:35:33 +0530

    Add controller AdminController and Publice Controller

--- src/main/java/SchoolManagementApp/Controller/AdminContoller.java
+++ src/main/java/SchoolManagementApp/Controller/AdminContoller.java
@@ -1,4 +1,26 @@
 package SchoolManagementApp.Controller;
 
+import SchoolManagementApp.DTO.UserEntityDtoSignup;
+import SchoolManagementApp.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/Admin")
 public class AdminContoller {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/get-All-users")
+    public ResponseEntity<?> getAllUsers(){
+        return new ResponseEntity<>(userService.getAllUsers(),HttpStatus.FOUND);
+
+    }
+
+
+
+
 }
--- src/main/java/SchoolManagementApp/Controller/publicContoller.java
+++ src/main/java/SchoolManagementApp/Controller/publicContoller.java
@@ -1,15 +1,29 @@
 package SchoolManagementApp.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import SchoolManagementApp.DTO.UserEntityDto;
+import SchoolManagementApp.DTO.UserEntityDtoSignup;
+import SchoolManagementApp.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/public")
 public class publicContoller {
 
+    @Autowired
+    UserService userService;
+
     @GetMapping("/health-check")
     public String checkHealthCheck(){
         return "OK";
+    }
+
+    @PostMapping("/create-users")
+    public ResponseEntity<?> createUser(@RequestBody UserEntityDtoSignup newUser){
+        userService.createUser(newUser);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }

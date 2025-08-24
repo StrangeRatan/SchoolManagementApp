58bab25fab2309b7827e0b9373fd4272fa07c15b
Ratan <ratanyadav405@gmail.com>
2025-08-24 11:41:56 +0530

    Create User Repository

--- src/main/java/SchoolManagementApp/Repository/UserEntityRepository.java
+++ src/main/java/SchoolManagementApp/Repository/UserEntityRepository.java
@@ -1,4 +1,9 @@
 package SchoolManagementApp.Repository;
 
-public interface UserEntityRepository {
+import SchoolManagementApp.Entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserEntityRepository extends JpaRepository<UserEntity,Long> {
 }
